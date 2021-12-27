0e53bed908ddb2604a81ff7642b338f8cbe5a896
Gigi <wegmarken2006@libero.it>
2021-12-27 22:01:24 +0100

    minimal example

---
+++ examples/minimal/go.mod
@@ -0,0 +1,12 @@
+module mini
+
+go 1.17
+
+require github.com/wegmarken2006/gwui v1.0.2
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
+	github.com/wegmarken2006/filepanic v1.0.2 // indirect
+	golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71 // indirect
+)
--- examples/minimal/mini.go
+++ examples/minimal/mini.go
@@ -1,12 +1,7 @@
-# gwui
-## UI library based on Bootstrap 5.1.3
-
-## Minimal example:
-```go
 package main
 
 import (
-    . "fmt"
+	. "fmt"
 
 	gw "github.com/wegmarken2006/gwui"
 )
@@ -37,23 +32,3 @@ func main() {
 
 	gc.GWWaitKeyFromCOnsole()
 }
-```
-
-## App folder structure:
-```
-|-myapp
-|   myapp.go
-|   go.mod
-|----static
-|    | web2.css
-|    |----bootstrap
-```
-## Copy the static folder from the [examples](./examples) here.
-## Optionally, use web2.css for further customizations.
-##
-## To compile the examples:
-```
-cd example\ksink
-go mod tidy
-go build .
-```
